8d32e1efe756dd738a128f320f7bd3935edb3fbb
juanlealr <juan.leal04@uptc.edu.co>
2026-05-02 17:30:35 -0500

    Arreglar variables de seguridad y de sistema

--- vetcita-backend/src/main/java/co/edu/vetcita/VetcitaApplication.java
+++ vetcita-backend/src/main/java/co/edu/vetcita/VetcitaApplication.java
@@ -3,10 +3,16 @@ package co.edu.vetcita;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class VetcitaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e -> 
+            System.setProperty(e.getKey(), e.getValue())
+        );
 		SpringApplication.run(VetcitaApplication.class, args);
 	}
 
